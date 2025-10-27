685dd36a9f7e30b813c14dca0db6e1ee1b975f0d
LinconRuthes <lincon.ruthes@gmail.com>
2025-10-27 01:00:53 -0300

    Multiplicar

--- test_calculadora.py
+++ test_calculadora.py
@@ -7,4 +7,6 @@ class TestCalculadora(unittest.TestCase):
 
     def test_subtrair_funciona(self):
         self.assertEqual(calculadora.subtrair(5, 3), 2)
-        
+    
+    def test_multiplicar_funciona(self):
+        self.assertEqual(calculadora.multiplicar(3, 4), 12)    
