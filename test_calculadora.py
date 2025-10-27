2861b800a84ccc1cce0c306498bcaba86addd7b5
LinconRuthes <lincon.ruthes@gmail.com>
2025-10-27 00:54:16 -0300

    feat: Adiciona teste para a funcao faltante e atinge 25% de cobertura

--- test_calculadora.py
+++ test_calculadora.py
@@ -4,7 +4,3 @@ import calculadora
 class TestCalculadora(unittest.TestCase):
     def test_somar_funciona(self):
         self.assertEqual(calculadora.somar(1, 2), 3)
-
-    # NOVO TESTE: Para cobrir a função subtrair
-    def test_subtrair_funciona(self):
-        self.assertEqual(calculadora.subtrair(5, 3), 2) # <-- Esta linha vai tocar o return de subtrair
