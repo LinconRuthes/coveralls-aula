0441a985582e9d440cef467a07061eb9350b4b77
LinconRuthes <lincon.ruthes@gmail.com>
2025-10-27 00:43:50 -0300

    feat: Adiciona teste para a funcao subtrair e atinge 100% de cobertura

--- test_calculadora.py
+++ test_calculadora.py
@@ -5,6 +5,6 @@ class TestCalculadora(unittest.TestCase):
     def test_somar_funciona(self):
         self.assertEqual(calculadora.somar(1, 2), 3)
 
-    # Note que a função 'subtrair' ainda não tem teste aqui! 
-    # Ela será a linha vermelha no Coveralls, gerando os 88%.
-    # Para o propósito da demonstração, não a incluímos.
+    # NOVO TESTE: Para cobrir a função subtrair
+    def test_subtrair_funciona(self):
+        self.assertEqual(calculadora.subtrair(5, 3), 2) # <-- Esta linha vai tocar o return de subtrair
