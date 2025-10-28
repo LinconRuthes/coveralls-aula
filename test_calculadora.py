f832ee0eefd9a4e87c08121daf444c797fef086d
LinconRuthes <lincon.ruthes@gmail.com>
2025-10-27 22:17:27 -0300

    Teste somar

--- test_calculadora.py
+++ test_calculadora.py
@@ -3,4 +3,4 @@ import calculadora
 
 class TestCalculadora(unittest.TestCase):
     def test_somar_funciona(self):
-        self.assertEqual(calculadora.somar(1, 2), 4)
+        self.assertEqual(calculadora.somar(1, 2), 3)
