1a259e6c5a239c8dfc0bed67c9306cf8ef85e758
LinconRuthes <lincon.ruthes@gmail.com>
2025-10-30 20:05:47 -0300

    ajuste soma

--- test_calculadora.py
+++ test_calculadora.py
@@ -5,11 +5,3 @@ class TestCalculadora(unittest.TestCase):
     def test_somar_funciona(self):
         self.assertEqual(calculadora.somar(1, 2), 3)
 
-    def test_subtrair_funciona(self):
-        self.assertEqual(calculadora.subtrair(3, 2), 1)
-
-    def test_multiplica_funciona(self):
-        self.assertEqual(calculadora.multiplicar(3, 2), 6)
-
-    def test_dividir_funciona(self):
-        self.assertEqual(calculadora.dividir(3, 3), 1)
