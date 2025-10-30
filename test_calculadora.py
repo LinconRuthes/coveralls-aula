a86fcea12b72e3c441db622bfc8dcab9ecd7ee8a
LinconRuthes <lincon.ruthes@gmail.com>
2025-10-30 20:01:48 -0300

    somar aula kleber

--- test_calculadora.py
+++ test_calculadora.py
@@ -7,3 +7,9 @@ class TestCalculadora(unittest.TestCase):
 
     def test_subtrair_funciona(self):
         self.assertEqual(calculadora.subtrair(3, 2), 1)
+
+    def test_multiplica_funciona(self):
+        self.assertEqual(calculadora.multiplicar(3, 2), 6)
+
+    def test_dividir_funciona(self):
+        self.assertEqual(calculadora.dividir(3, 3), 1)
