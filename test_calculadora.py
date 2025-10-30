405b53cc0d7f26b70f6deac615d28a21a36c49d1
LinconRuthes <lincon.ruthes@gmail.com>
2025-10-30 20:08:10 -0300

    teste errado

--- test_calculadora.py
+++ test_calculadora.py
@@ -3,5 +3,5 @@ import calculadora
 
 class TestCalculadora(unittest.TestCase):
     def test_somar_funciona(self):
-        self.assertEqual(calculadora.somar(1, 2), 3)
+        self.assertEqual(calculadora.somar(1, 2), 8)
 
